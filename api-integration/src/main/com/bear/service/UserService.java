724f33b66fd1e5428dc75d835a0a374d04f4ecd5
zhangxiongd <zhangxiongd@gmail.com>
2017-04-12 22:45:12 +0800

    add dubbo function

---
+++ api-integration/src/main/com/bear/service/UserService.java
@@ -0,0 +1,8 @@
+package com.bear.service;
+
+/**
+ * Created by zhangxiong on 2017-04-12.
+ */
+public interface UserService {
+    String getName(String naem);
+}
---
+++ service-integration/src/main/java/com/bear/facade/UserServiceFacade.java
@@ -0,0 +1,7 @@
+package com.bear.facade;
+
+/**
+ * Created by zhangxiong on 2017-04-12.
+ */
+public class UserServiceFacade {
+}
